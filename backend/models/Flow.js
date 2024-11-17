5d945ad774fdf84c5b5a243eb212906d90a28234
vatsal <vatsal3064@gmail.com>
2024-11-18 02:06:01 +0530

    implemented frontend v0

--- backend/models/Flow.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const flowSchema = new mongoose.Schema({
-  status: { type: String, default: 'Started' },
-  log: [{ type: String }],
-});
-
-module.exports = mongoose.model('Flow', flowSchema);
